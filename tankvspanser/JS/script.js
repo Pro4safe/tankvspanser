aa3b495a1dff44d9a936dd6ff3bd2329954dd1e4
Pro4safe <weaztinthd@gmail.com>
2014-11-07 13:53:25 +0100

    work

--- tankvspanser/JS/script.js
+++ tankvspanser/JS/script.js
@@ -4,7 +4,12 @@ var p1x,
     p1vx,
     p2x, 
     p2y,
-    p2vx;
+    p2vx,
+    s1x,
+    s1y,
+    s1vx,
+    s1vy,
+    say;
 
 function start(){
     c = document.getElementById("duk");
@@ -21,6 +26,11 @@ function start(){
     p2x= c.width * 0.9; 
         p2y=c.height * 0.8; 
         p2vx=0;
+        s1x=100;
+        s1y=100;
+        s1vx=2;
+        s1vy=-3;
+        say= 0.05;
 
 }
 
@@ -35,6 +45,9 @@ function updatePositions(){
     p1x += p1vx;
     p1x += p1vx;
     p2x += p2vx;
+    s1x += s1vx;
+    s1y += s1vy;
+    s1vy += say;
 }
 
 function repaint(){
@@ -42,6 +55,8 @@ function repaint(){
     ctx.clearRect(0,0,c.width, c.height);
     paintTank(p1x, p1y);
     paintTank(p2x, p2y);
+    
+    paintShot(s1x,s1y);
 }
 
 function paintTank(x, y){
@@ -49,6 +64,14 @@ function paintTank(x, y){
     ctx.fillRect(x, y, c.width * 0.05, c.height * 0.05);
     
 }
+
+function paintShot(x, y){
+    ctx.beginPath();
+    ctx.arc(x, y, 3, 0, 2 * Math.PI);
+    ctx.fill();
+    ctx.closePath();
+}
+
 
 function keyDown(e){
     e.preventDefault();
@@ -59,7 +82,15 @@ if(e.keyCode==37){
     
     }
 if(e.keyCode==39){
-    p2vx = c.width * 0.0005
+    p2vx = c.width * 0.0005;
+    }
+
+if(e.keyCode==65){
+    p1vx= -c.width * 0.0005;
+    
+    }
+if(e.keyCode==68){
+    p1vx = c.width * 0.001;
     }
 
 }
@@ -68,4 +99,11 @@ function keyUp(e){
     if (e.keyCode==37 || e.keyCode==39){
     p2vx= 0;
     }
+    if (e.keyCode==65 || e.keyCode==68){
+    p1vx= 0;
+    }
+
 }
+
+
+
