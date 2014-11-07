aa123dddf5a68bccc514aa7d38d2d5d2cd71e597
Pro4safe <weaztinthd@gmail.com>
2014-11-07 13:24:58 +0100

    tank

---
+++ tankvspanser/JS/script.js
@@ -0,0 +1,71 @@
+var c , ctx;
+var p1x, 
+    p1y, 
+    p1vx,
+    p2x, 
+    p2y,
+    p2vx;
+
+function start(){
+    c = document.getElementById("duk");
+    ctx= c.getContext("2d");
+    
+    c.width= window.innerWidth;
+    c.height= window.innerHeight;
+    
+    window.setInterval(update, 20);
+    
+    p1x=c.width * 0.1;
+        p1y= c.height * 0.8;
+        p1vx=0;
+    p2x= c.width * 0.9; 
+        p2y=c.height * 0.8; 
+        p2vx=0;
+
+}
+
+function update(){
+    updatePositions();
+    repaint();
+
+
+}
+
+function updatePositions(){
+    p1x += p1vx;
+    p1x += p1vx;
+    p2x += p2vx;
+}
+
+function repaint(){
+    
+    ctx.clearRect(0,0,c.width, c.height);
+    paintTank(p1x, p1y);
+    paintTank(p2x, p2y);
+}
+
+function paintTank(x, y){
+    
+    ctx.fillRect(x, y, c.width * 0.05, c.height * 0.05);
+    
+}
+
+function keyDown(e){
+    e.preventDefault();
+    
+    
+if(e.keyCode==37){
+    p2vx= -c.width * 0.001;
+    
+    }
+if(e.keyCode==39){
+    p2vx = c.width * 0.0005
+    }
+
+}
+
+function keyUp(e){
+    if (e.keyCode==37 || e.keyCode==39){
+    p2vx= 0;
+    }
+}
