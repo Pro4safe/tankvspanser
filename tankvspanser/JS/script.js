c7f047870430dbe703c99fc9182d9b653bf79f57
Pro4safe <weaztinthd@gmail.com>
2015-03-27 16:07:37 +0100

    change arck of fire

--- tankvspanser/JS/script.js
+++ tankvspanser/JS/script.js
@@ -1,21 +1,13 @@
 var c, ctx, tankimg, tank1img, backimg;
 var p1x, p1y, p1vx, p2x, p2y, p2vx, s1x, s1y, s1vx, s1vy, s2x, s2y, s2vx, s2vy, say,
-    f1x = -40,
-    f1y = -40,
-    f2x = -40,
-    f2y = -40,
-    p1h = 100,
-    p2h = 100,
-    p1l = 3,
-    p2l = 3,
-    k1vy = -8,
-    name1,
-    name2;
+    f1x = -40, f1y = -40, f2x = -40, f2y = -40, p1h = 100, p2h = 100, p1l = 3, p2l = 3, k1vy = -8, k2vy = -4, Player1,
+    Player2;
+var heliimg, h1x=200, h1y=100, h1vx=0, h1vy=0;
 
 function start() {
   
-    name1 = prompt("Player 1");
-    name2 = prompt("Player 2");
+    Player1 = prompt("Player 1");
+    Player2 = prompt("Player 2");
     
     c = document.getElementById("duk");
     ctx = c.getContext("2d");
@@ -23,6 +15,7 @@ function start() {
     tankimg = document.getElementById("tankimg");
     tank1img = document.getElementById("tank1img");
     exploimg = document.getElementById("exploimg");
+    heliimg = document.getElementById("heliimg"); 
     
     c.width = 1000;
     c.height = 600;
@@ -59,7 +52,6 @@ function update() {
 
 function updatePositions() {
     p1x += p1vx;
-    p1x += p1vx;
 
     p2x += p2vx;
 
@@ -76,7 +68,7 @@ function updatePositions() {
         f1x = p1x;
         f1y = p1y;
 
-        p1h -= 30;
+        p1h -= 20;
 
         s2y = c.height;
         if (p1h < 0) {
@@ -91,7 +83,7 @@ function updatePositions() {
         f2x = p2x;
         f2y = p2y;
 
-        p2h -= 30;
+        p2h -= 20;
 
         s1y = c.height;
         if (p2h < 0) {
@@ -110,19 +102,19 @@ function repaint() {
     ctx.clearRect(0, 0, c.width, c.height);
     paintTank(p1x, p1y, tank1img);
     paintTank(p2x, p2y, tankimg);
-
+    paintHeli(h1x, h1y, heliimg); 
     paintShot(s1x, s1y);
     paintShot(s2x, s2y);
 
     paintFire(f1x, f1y);
     paintFire(f2x, f2y);
-
+    
     paintInfo(tank1img);
     
     ctx.font = "18px Arial";
-    ctx.fillText(name1,850,95);
+    ctx.fillText(Player1,850,95);
      ctx.font = "18px Arial";
-    ctx.fillText(name2,850,115);
+    ctx.fillText(Player2,850,115);
 
 }
 
@@ -130,6 +122,10 @@ function paintTank(x, y, img) {
 
     ctx.drawImage(img, x, y, c.width * 0.05, c.height * 0.05);
 
+}
+
+function paintHeli(x, y, img){
+ctx.drawImage(img, x, y);
 }
 
 function Turnover(){
@@ -178,7 +174,7 @@ function keyDown(e) {
     if (e.keyCode == 39) {
         p2vx = c.width * 0.0005;
     }
-    //vänster pl1a
+    //vänster pl1 a
     if (e.keyCode == 65) {
         p1vx = -c.width * 0.0005;
 
@@ -202,26 +198,38 @@ function keyDown(e) {
         s2x = p2x;
         s2y = p2y;
         s2vx = -2;
-        s2vy = -3;
+        s2vy = k2vy;
     }
     //rotera tornet öka pil up
-    if(e.keyCode == 40){
-    
-        k1vy = 0;
-        
-    }
-    //rotera tornet minska .pil ner
     if(e.keyCode == 38){
     
         
-        k1vy = -8;
+        k1vy -= 1;
+    }
+    //rotera tornet minska .pil ner
+    if(e.keyCode == 40){
+    
+        
+        k1vy += 1;
     }
     //styrka på skott öka w
     if(e.keyCode == 87){
-    
+        k2vy -= 1;
     }
     //styrka på skott minska s
     if(e.keyCode == 83){
+        k2vy += 1;
+    }
+    //helicopter move right j
+    if(e.keyCode == 71)
+    //helicopter move left g
+    if(e.keyCode == 74)
+    //helicopter shot left target z
+    if(e.keyCode == 88){
+    
+    }
+    //helicopter shot on right trarget x
+    if(e.keyCode == 90){
     
     }
     
